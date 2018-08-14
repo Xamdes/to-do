dfbe38b1d9be635d85ac575225c088e4b2714cd1
Steven Colburn <stevencolburn@xamdes.com>
2018-08-14 16:11:26 -0700

    working fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,14 @@
+// import { Component } from '@angular/core';
+//
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'app';
+// }
+
 import { Component } from '@angular/core';
 import { Task } from './models/task.model';
 
