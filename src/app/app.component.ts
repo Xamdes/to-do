e92ba7f5dc8855724241822ee6e0fe87365c548c
Steven Colburn <stevencolburn@xamdes.com>
2018-08-15 09:42:47 -0700

    add username input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,3 @@
-// import { Component } from '@angular/core';
-//
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'app';
-// }
-
 import { Component } from '@angular/core';
 import { Task } from './models/task.model';
 
@@ -17,16 +6,35 @@ import { Task } from './models/task.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent
+{
   currentFocus: string = 'Angular Homework';
   currentTime: Date = new Date();
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+    new Task('Finish weekend Angular homework for Epicodus course',3),
+    new Task('Begin brainstorming possible JavaScript group projects',2),
+    new Task('Add README file to last few Angular repos on GitHub',2)
   ];
+  selectedTask: Task = this.tasks[0];
+
+  EditTask()  {
+    alert("Time to edit!");
+  }
+
+  PriorityColor(currentTask)
+  {
+    switch(currentTask.priority)
+    {
+      case 2:
+      return "bg-warning";
+      case 3:
+      return "bg-danger";
+      default:
+      return "bg-info";
+    }
+  }
 
 }
