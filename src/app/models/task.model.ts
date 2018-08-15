5d9689ad28a0ceedc49191af924ad19f9781fe08
Steven Colburn <stevencolburn@xamdes.com>
2018-08-15 15:37:28 -0700

    more styling

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -2,4 +2,13 @@ export class Task
 {
   public done: boolean = false;
   constructor(public description: string, public priority: number) { }
+
+  TaskDone()
+  {
+    if(this.done)
+    {
+      return "Yes";
+    }
+    return "No";
+  }
 }
